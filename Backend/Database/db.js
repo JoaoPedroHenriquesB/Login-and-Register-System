aaed5154158d1b1b1ca65e874778a8e064512db0
JoaoPedroHenriquesB <joaopedrohbalbino@gmail.com>
2025-07-22 13:03:04 -0400

    update to run in Render

--- Backend/Database/db.js
+++ Backend/Database/db.js
@@ -1,10 +1,9 @@
 const mysql = require('mysql2');
-
 const db = mysql.createConnection({
-    host: 'host name',
-    user: 'username',
-    password: 'pass',
-    database: 'database name'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
